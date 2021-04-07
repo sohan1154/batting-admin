61fa636bc2a9205494e10a8c8f73ad8a75b3abee
Sohan Lal Saini <sohan1154@gmail.com>
2021-04-07 14:08:36 +0530

    basic configuration changed

--- src/providers/globals/config.js
+++ src/providers/globals/config.js
@@ -3,7 +3,8 @@ export const appName = "Bat in Vegas";
 
 export const perPage = 10;
 
-export const baseURL = 'http://172.104.41.239:3001';
-
+// export const baseURL = 'http://172.104.41.239:3001';
 // export const socketURL = 'http://172.104.41.239:3002';
+
+export const baseURL = 'http://localhost:3001';
 export const socketURL = 'http://localhost:3002';
